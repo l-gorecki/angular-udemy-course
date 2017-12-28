7409b2bf3968d9b4fde5b825245291fd3c791f83
l-gorecki <gorecki.lukas@gmail.com>
2017-12-28 11:04:33 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  paragraphVisible = false;
+  buttonClicks = [];
+  toggleButton() {
+    this.paragraphVisible = !this.paragraphVisible;
+    this.buttonClicks.push(new Date());
+  }
 }
