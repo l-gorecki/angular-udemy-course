312865fb4d006ba6ff21447d3c3338cabdee23dc
l-gorecki <gorecki.lukas@gmail.com>
2017-12-30 09:53:49 +0100

    accesing viechild, contentchild in lifecycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, OnDestroy, ViewChild, ElementRef, AfterViewInit, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,9 +6,12 @@ import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, 
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // native is shadowDOM
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, OnDestroy {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, OnDestroy, AfterViewInit {
+  // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') para: ElementRef;
 
   constructor() { console.log('constructor called'); }
 
@@ -21,6 +24,8 @@ ngOnChanges(changes: SimpleChanges) {
 
   ngOnInit() {
     console.log('ngOnInit called');
+    console.log('text content (onInit): ' + this.header.nativeElement.textContent);
+    console.log('paragraph content (onInit): ' + this.para.nativeElement.textContent);
   }
 
   ngDoCheck() {
@@ -28,6 +33,13 @@ ngOnChanges(changes: SimpleChanges) {
   }
   ngAfterContentInit() {
     console.log('ngAfterContentInit called');
+    console.log('text content (afterContentInit): ' + this.header.nativeElement.textContent);
+    console.log('paragraph content (afterContentInit): ' + this.para.nativeElement.textContent);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    console.log('text content (afterViewInit): ' + this.header.nativeElement.textContent);
   }
 
   // etc
