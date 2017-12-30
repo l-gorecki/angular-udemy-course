915caa4e8f926fa2b561cd9956d4a626c597d532
l-gorecki <gorecki.lukas@gmail.com>
2017-12-30 10:03:41 +0100

    naive navigation (no router)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  selectedMenuItem = 'recipe';
+
+  onMenuSelected(menu: string) {
+    this.selectedMenuItem = menu;
+  }
 }
