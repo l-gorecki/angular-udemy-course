fc257f4e18473ea233458caa73bfc85c16440eba
l-gorecki <gorecki.lukas@gmail.com>
2017-12-31 08:40:26 +0100

    host binding

--- src/app/highlight/highlight.directive.ts
+++ src/app/highlight/highlight.directive.ts
@@ -1,15 +1,19 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+
   @HostListener('mouseenter') mouseEnter(envetData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseLeave(envetData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
