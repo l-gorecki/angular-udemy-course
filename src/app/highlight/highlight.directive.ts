f0e5d8e99ee184e853336f06c620a7695b3ff12e
l-gorecki <gorecki.lukas@gmail.com>
2017-12-31 08:47:21 +0100

    finished attribute directive

--- src/app/highlight/highlight.directive.ts
+++ src/app/highlight/highlight.directive.ts
@@ -1,23 +1,28 @@
-import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @Input() defaultColor = 'transparent';
+  @Input('appHighlight') highlightColor = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor = this.defaultColor;
 
   @HostListener('mouseenter') mouseEnter(envetData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(envetData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
+   }
 
 }
