4a036f942a474dae8ce02b477ad0e1af8cf672e4
l-gorecki <gorecki.lukas@gmail.com>
2018-01-07 08:46:02 +0100

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { UsersService } from './users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user1activated = false;
+  user2activated = false;
+
+  constructor(private userServ: UsersService) {}
+
+  ngOnInit() {
+    this.userServ.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1activated = true;
+        } else if (id === 2) {
+          this.user2activated = true;
+        }
+      }
+    );
+  }
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { setTimeout } from 'timers';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  numberObsSub: Subscription;
+  obsSub: Subscription;
   constructor() { }
 
   ngOnInit() {
+    const numberObs = Observable.interval(1000)
+      .map(
+        (data: number) => data * 2
+      );
+    this.numberObsSub = numberObs.subscribe(
+      (number: number) => { console.log(number); }
+    );
+    const obs = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('1st package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('2nd package');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('error package');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('3rd package');
+      }, 6000);
+    });
+
+    this.obsSub = obs.subscribe(
+      (data: string) => {console.log(data); },
+      (error: string) => {console.error(error); },
+      () => { console.info('completed'); }
+    );
+  }
+
+  ngOnDestroy() {
+    this.obsSub.unsubscribe();
+    this.numberObsSub.unsubscribe();
   }
 
 }
